e8a33017216a323dde8f312815519cd3067c5447
779g <rdp@779.mx>
2020-01-25 00:38:28 +0000

    added config.js to stories, to use with genstories 1.1.1

---
+++ src/stories/config.js
@@ -0,0 +1,49 @@
+const SIZES=[
+  'xs',
+  'sm',
+  'md',
+  'lg',
+  'xl',
+  'xxl'
+]
+
+const THEME = [
+  'primary',
+  'secondary',
+  'accent1',
+  'accent2',
+  'accent3'
+]
+
+const WHEEL = [
+  'red',
+  'orange',
+  'yellow',
+  'olive',
+  'green',
+  'indigo',
+  'azure',
+  'blue',
+  'violet',
+  'pink'
+]
+
+const MODALS = [
+  'success',
+  'warning',
+  'error'
+]
+
+const ALL_COLORS = [
+  ...THEME,
+  ...WHEEL,
+  ...MODALS
+]
+
+export {
+  SIZES,
+  THEME,
+  WHEEL,
+  MODALS,
+  ALL_COLORS
+}
